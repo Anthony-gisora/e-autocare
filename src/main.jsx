5a7336609af9dfb1d7fe16cf97ebbfcbee5b0e5d
Tonny_G <gesoranthony@gmail.com>
2025-07-02 14:18:35 +0300

    Changes on the svg and home page

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-react";
+import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
 
 const PUBLISHED_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHED_KEY) {
   throw new Error("Encountered a problem with the clark key...! ");
 } else {
-  console.log("Clerk");
+  console.log("Clerk in sync...!");
 }
 
 createRoot(document.getElementById("root")).render(

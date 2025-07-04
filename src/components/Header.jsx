aef83ccf7f24e15f9b5251aae9d6d72ff09ced9c
Tonny_G <gesoranthony@gmail.com>
2025-07-04 14:31:32 +0300

    Updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -57,14 +57,7 @@ const Header = () => {
                 About Us
               </button>
             </li>
-            <li>
-              <button
-                onClick={() => navigate("/switch")}
-                className="block py-2 px-4 text-[#2b2d42] hover:underline cursor-pointer"
-              >
-                Switch Account
-              </button>
-            </li>
+
             <li>
               <button
                 onClick={handleSignOut}
